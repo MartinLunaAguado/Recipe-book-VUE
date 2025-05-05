56d8e8258e8bf95166f7a812cbc15a331a54d6ef
Martin Luna Aguado <m.laguado@alumnos.upm.es>
2025-05-05 23:18:40 +0200

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router';
+ import HomeView from '../views/HomeView.vue';
+ import FavoritesView from '@/views/FavoritesView.vue';
+ import AddRecipeView from '@/views/AddRecipeView.vue';
+
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+       path: '/favorites',
+       name: 'favorites',
+       component: FavoritesView
+     },
+     {
+       path: '/add-recipe',
+       name: 'add-recipe',
+       component: AddRecipeView
+    },
+  ],
+})
+
+export default router;
