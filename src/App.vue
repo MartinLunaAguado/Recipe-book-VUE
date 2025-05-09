18579557286ba11b8ccf67ae6863908c5e319927
Martin Luna Aguado <m.laguado@alumnos.upm.es>
2025-05-09 10:47:21 +0200

    fallo con tailwindcss tengo que cambiar todo el css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <header>
-    <nav>
-      <ul class="flex gap-4 p-8 bg-orange-200 text-orange-800">
+    <nav class="flex items-center justify-between bg-orange-600 text-white p-4 shadow-lg">
+      <div class="text-2xl font-bold">
+        <RouterLink :to="{ name: 'home' }" class="hover:text-orange-200">🍴 Recipe Book</RouterLink>
+      </div>
+      <ul class="flex gap-14 mr-16">
         <li>
-          <RouterLink :to="{ name: 'home', query:{ id: 1} }" class="hover:underline">Home</RouterLink>
+          <RouterLink :to="{ name: 'home' }" class="hover:underline">Home</RouterLink>
         </li>
         <li>
           <RouterLink :to="{ name: 'favorites' }" class="hover:underline">Favorites</RouterLink>
@@ -15,7 +18,7 @@
     </nav>
   </header>
 
-  <main class="bg-orange-100 h-screen p-4">
+  <main class="bg-gray-100 min-h-screen py-8">
     <RouterView />
   </main>
 </template>
